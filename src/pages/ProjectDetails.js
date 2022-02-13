d434a7953db032fc853283c3733ec9f67234a627
diegomarrufo <dmarrufo@ucm.es>
2022-02-13 14:46:12 +0100

    changed the buttons for project details

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -11,13 +11,6 @@ const ProjectDetails = (props) => {
 
 	return (
 		<div className="grid grid-cols 1 lg:grid-cols-2 shadow-xl rounded-xl">
-			<div>
-				<img
-					src={require("../images/" + image_path + ".jpg")}
-					alt={title + ". " + subtitle}
-					className="h-full w-full animate-appear2 shadow-lg rounded-lg xl:rounded-lg aspect-2"
-				/>
-			</div>
 			<div className="flex flex-col justify-center ml-10">
 				<h2>{title}</h2>
 				<h3>{subtitle}</h3>
@@ -30,6 +23,13 @@ const ProjectDetails = (props) => {
 					</a>
 				</div>
 				<ul>{tagsList}</ul>
+			</div>
+			<div>
+				<img
+					src={require("../images/" + image_path + ".jpg")}
+					alt={title + ". " + subtitle}
+					className="h-full w-full animate-appear2 shadow-lg rounded-lg xl:rounded-lg aspect-2"
+				/>
 			</div>
 		</div>
 	);
