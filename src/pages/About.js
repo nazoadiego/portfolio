0960ece2b394e729077d1654a5422bd924e23ede
diegomarrufo <dmarrufo@ucm.es>
2022-02-25 22:48:54 +0100

    gst added contact form functionality using emailjs

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import DiegoIs from "../components/DiegoIs";
 import Education from "../components/Education";
 import Technologies from "../components/Technologies";
 import Courses from "../components/Courses";
+import Contact from "../pages/Contact";
 
 const AboutPage = () => {
 	return (
@@ -15,6 +16,7 @@ const AboutPage = () => {
 				<div className="mt-4 prose lg:prose-xl dark:prose-invert">
 					<Technologies />
 					<Courses />
+					<Contact />
 				</div>
 			</div>
 		</>
---
+++ src/pages/Contact.js
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+	const form = useRef();
+	const [result, setResult] = useState(false);
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_ekc86h8",
+				"template_9br42vb",
+				form.current,
+				"user_SCWIx9ULODUFZodDiR0bl"
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+		e.target.reset();
+		setResult(true);
+	};
+
+	return (
+		<form ref={form} onSubmit={sendEmail} className="flex flex-col">
+			<label>Name</label>
+			<input type="text" name="from_name" />
+			<label>Email</label>
+			<input type="email" name="email" />
+			<label>Message</label>
+			<textarea name="message" />
+			<input type="submit" value="Send" />
+			{result && <h6>Your message has been sent. I will contact you soon.</h6>}
+		</form>
+	);
+};
+
+export default Contact;
