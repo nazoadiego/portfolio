b602da59a1bd91d9a8f2d2713be840a4867decbe
diegomarrufo <dmarrufo@ucm.es>
2022-03-11 17:15:19 +0100

    fixed padding/margin issue when button is white

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -25,7 +25,9 @@ const Filter = (props) => {
 					setActiveTag("");
 				}}
 				className={
-					activeTag === "" ? "btn-blue mt-2 ml-2 lg:ml-0" : "btn-white"
+					activeTag === ""
+						? "btn-blue mt-2 ml-2 lg:ml-0"
+						: "btn-white mt-2 ml-2 lg:ml-0"
 				}
 			>
 				All
