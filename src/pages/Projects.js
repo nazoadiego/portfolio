9965a804d29f0c3dc4cec50efc6728c2797b95d3
diegomarrufo <dmarrufo@ucm.es>
2022-03-11 17:14:22 +0100

    added typescript to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -53,3 +53,49 @@ const ProjectsPage = () => {
 };
 
 export default ProjectsPage;
+
+import React, { Component } from "react";
+import './reload.scss';
+import axios from 'axios';
+
+
+class Reload extends Component {
+    constructor(props){
+    super(props);
+    this.handleNext.bind(this)
+  }
+
+  handleNext = e => {
+    e.preventDefault();
+    const s = axios.get(`https://jsonplaceholder.typicode.com/posts`)
+        .then(res => {
+      var data = res.data;
+
+      const s = data.map(element => {
+        var m = 0
+          for(element.id = m; element.id < m; element.id ++ ){
+            element.id += 1
+
+          }
+            console.log(element.id);
+          // }
+      });
+
+    })
+  };
+
+  render(){
+    return(
+      <div className="buttons">
+        <div className="previous">
+          {/* <h2 onClick={this.handlePrevious}>Previous</h2> */}
+        </div>
+        <div className="next">
+          <h2 onClick={this.handleNext}>Next</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Reload;
